14cc21847b9de96cc488249c7b44787352b46925
Albert Lee <alee@counsyl.com>
2018-08-17 10:12:25 -0700

    Add git related productivity tools

--- git_changed_files.sh
+++ git_changed_files.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 
-LIST_OF_FILES=$(git log --oneline --decorate --graph --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(dim white) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)" --name-only master.. | grep '/'| grep -v '*'| egrep -v '\|\s+$' | awk '{print $2}'| sed '/^$/d' | sort | uniq)
+BRANCH_TO_COMPARE_TO="$1"
+LIST_OF_FILES=$(git log --oneline --decorate --graph --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(dim white) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)" --name-only ${BRANCH_TO_COMPARE_TO}.. | grep -v '*'| egrep -v '\|\s+$' | awk '{print $2}'| sed '/^$/d' | sort | uniq)
 
 #echo "Modifies Files:"
 #echo "---------------"
 
-for i in $LIST_OF_FILES; do 
+for i in $LIST_OF_FILES; do
     test -f $i && echo "$i"
 done
