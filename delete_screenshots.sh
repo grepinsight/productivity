c0f75856588d6e96ff870700760ddedf4c9d6b9a
Albert Lee <alee@counsyl.com>
2019-07-11 11:19:22 -0700

    delete_screenshots

--- delete_screenshots.sh
+++ delete_screenshots.sh
@@ -5,6 +5,6 @@ set -eux
 
 contents=$(find /Users/allee/Desktop/ -type f -name 'Screen*png' -print0)
 
-if [[ ! -z $contents ]]; then
+if [[ -n "$contents" ]]; then
 	find ~/Desktop/ -type f -name "Screen*png" -print0 | xargs -0 rm
 fi
