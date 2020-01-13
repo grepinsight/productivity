8de88a8134c8e488347e9ab9b8be4b18d703ece1
Albert Lee <grepinsight@gmail.com>
2020-01-13 10:30:39 -0800

    Update

--- delete_screenshots.sh
+++ delete_screenshots.sh
@@ -5,6 +5,8 @@ set -eux
 
 contents=$(find /Users/allee/Desktop/ -type f -name 'Screen*png' -print0)
 
+mkdir -p $HOME/Desktop/screenshots
+
 if [[ -n "$contents" ]]; then
-	find ~/Desktop/ -type f -name "Screen*png" -print0 | xargs -0 rm
+	find ~/Desktop/ -type f -name "Screen*png" -print0 | xargs -0 -I{} mv {} $HOME/Desktop/screenshots/
 fi
